f0cd0a89adc966f5756450c23bc1b428baf1574c
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-10-23 16:34:59 +0100

    Add getProject/updateProject/deleteProject controllers

--- controllers/projectsControllers.js
+++ controllers/projectsControllers.js
@@ -1,4 +1,5 @@
 const Project = require('../models/projectModel');
+const mongoose = require('mongoose');
 
 const getProjects = async (req, res) => {
     const projects = await Project.find({}).sort({createdAt: -1});
@@ -46,4 +47,103 @@ const createProject = async (req, res) => {
     }
 }
 
-module.exports = {getProjects, createProject}
+const getProject = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'Invalid Id'
+                  })
+    }
+
+    const project = await Project.findById(id);
+
+    if(!project) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'No such project was found!'
+                  })
+    }
+
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                project
+            }
+        })
+}
+
+const updateProject = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'Invalid id'
+                  })
+    }
+
+    const project = await Project.findOneAndUpdate({_id: id}, {...req.body}, {new: true});
+
+    if(!project) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'No such project was found!'
+                  })
+    }
+
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                project
+            }
+        })
+}
+
+const deleteProject = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'Invalid Id'
+                  })
+    }
+
+    const project = await Project.findOneAndDelete({_id: id});
+
+    if(!project) {
+        return res
+                  .status(400)
+                  .json({
+                    status: 'fail',
+                    message: 'No such project was found!'
+                  })
+    }
+
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                project
+            }
+        })
+}
+
+module.exports = {getProjects, createProject, getProject, updateProject, deleteProject}
--- routes/projects.js
+++ routes/projects.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getProjects, createProject } = require('../controllers/projectsControllers');
+const { getProjects, createProject, getProject, updateProject, deleteProject } = require('../controllers/projectsControllers');
 
 const router = express.Router();
 
 router
     .get('/', getProjects)
     .post('/', createProject)
+    .get('/:id', getProject)
+    .patch('/:id', updateProject)
+    .delete('/:id', deleteProject)
 
 module.exports = router
